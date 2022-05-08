3972f1693a89a89301e3b5e146e57f1552d89adb
peternkane <peternkane24@gmail.com>
2022-05-08 13:27:47 +0300

    README FILE

---
+++ lib/answers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Answers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+          color: Colors.blue, child: Text('Anwer 1'), onPressed: null),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import 'answers.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestions() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favorite movie',
+      'what\'s your favorite series',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(questions[questionIndex]),
+            Answers(),
+            Answers(),
+            Answers(),
+          ],
+        ),
+      ),
+    );
+  }
+}
